4c64f76feb927fedae08811c1b251d21271c77e8
Tom Manderson <me@trm.io>
2019-05-30 18:24:04 +1000

    add notebook and update setup

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@ pip install -r requirements.txt
 jupyter contrib nbextension install --sys-prefix
 jupyter nbextensions_configurator enable
 jupyter-nbextension install rise --py --sys-prefix
+jupyter-nbextension enable freeze/main
+jupyter-nbextension enable init_cell/main
+jupyter-nbextension enable rise/main
